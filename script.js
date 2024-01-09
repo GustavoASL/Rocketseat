e811eea48195a2dc4ed35556ac897ba743d9d98c
Gustavo Ale <gustavo.alexandredj@gmail.com>
2024-01-09 19:01:06 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,28 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("dark")
+
+  // pegar a tag img
+  const img = document.querySelector("#profile img")
+
+  // substituir a imagem
+  if (html.classList.contains("dark")) {
+    // se tiver light mode, adicionar a imagem light
+    img.setAttribute("src", "./assets/avatar.png")
+  } else {
+    // se tiver sem light mode, manter a imagem normal
+    img.setAttribute("src", "./assets/avatar-light.png")
+  }
+
+  if (html.classList.contains("dark")) {
+    img.setAttribute(
+      "alt",
+      "Foto de Mayk Brito sorrindo, usando óculos e camisa preta, barba e fundo amarelo"
+    )
+  } else {
+    img.setAttribute(
+      "alt",
+      "Foto de Mayk Brito sorrindo, usando óculos escuro e camisa preta, sem barba e fundo azul"
+    )
+  }
+}
